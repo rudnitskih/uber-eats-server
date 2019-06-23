30eb0dfac6467458db6c9c23918e3a50c448ac45
Dmytro Rudnytskykh <rudnitskih@gmail.com>
2019-06-23 14:32:30 +0300

    Improve endpoints

--- routes/api.js
+++ routes/api.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const fs = require('fs');
-const marketplaces = require('../data/marketplaces.json');
+const restaurants = require('../data/restaurants.json');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/marketplaces', function(req, res) {
-  res.json(marketplaces);
+router.get('/restaurants', function(req, res) {
+  res.json(restaurants);
 });
 
-router.get('/store/:id', function(req, res) {
-  fs.readFile(`./data/stores/${req.params.id}.json`, 'utf-8', (err, data) => {
+router.get('/restaurants/:id', function(req, res) {
+  fs.readFile(`./data/restaurants/${req.params.id}.json`, 'utf-8', (err, data) => {
     if (err) throw err;
 
     res.json(JSON.parse(data));
