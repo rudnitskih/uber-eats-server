597d2517c98fb16bf7cf2fbe9d74f76359a2283d
Dmytro Rudnytskykh <rudnitskih@gmail.com>
2019-07-01 15:08:05 +0300

    Fix cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const api = require('./routes/api');
 
@@ -18,6 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'scraping')));
+app.use(cors());
 
 app.use('/api/v1', api);
 
